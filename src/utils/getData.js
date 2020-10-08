f9d39bc5fe21da0d8aeb91812df01563d5154762
Aqua-20 <Aqua-20@users.noreply.github.com>
2020-10-08 12:38:25 -0500

    Render personajes y logica de peticion a la API

---
+++ src/utils/getData.js
@@ -0,0 +1,14 @@
+const API = "http://rickandmortyapi.com/api/character/"
+
+const getData = async (id) => {
+  const apiUrl = id ? `${API}${id}` : API;
+  try{
+    const response = await fetch(apiUrl);
+    const data = response.json();
+    return data;
+  }catch(error){
+    console.log('Fetch Error', error)
+  };
+}
+
+export default getData
